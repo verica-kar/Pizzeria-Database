a1e3197f480181ba1e69a776db64576f895ab12a
Verica Karanakova <77712531+verica-kar@users.noreply.github.com>
2026-01-19 11:56:14 -0600

    upload files

---
+++ pizzeria queries/order_activity/order_activity_query.sql
@@ -0,0 +1,18 @@
+
+USE pizza_db;
+SELECT 
+	o.order_id,
+	i.item_price,
+	o.quantity,
+	i.item_cat,
+	i.item_name,
+	o.created_at,
+	a.delivery_address1,
+	a.delivery_address2,
+	a.delivery_city,
+	a.delivery_zipcode,
+	o.delivery
+FROM 
+	orders o
+	LEFT JOIN item i on o.item_id = i.item_id
+	LEFT JOIN address a on o.add_id = a.add_id
